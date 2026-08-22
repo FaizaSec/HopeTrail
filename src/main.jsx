ff0c4ca6753f3b98a0993a23f6ff13c161753039
attojakarobi <karobiattoja@gmail.com>
2026-08-22 11:56:20 +0600

    Used petgrid instead of dog or catGrid

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import DogGrid from "./components/DogGrid.jsx";
-import CatGrid from "./components/CatGrid.jsx";
+import PetGrid from "./components/PetGrid.jsx";
 import PetDetails from "./components/PetDetails.jsx";
 
 createRoot(document.getElementById("root")).render(
